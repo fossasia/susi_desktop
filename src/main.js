2b15db868a46868595ec97713e119b55bd05e9b7
Aditya Giri <dtrg21@gmail.com>
2017-01-19 06:57:33 +0530

    Add style to the app

--- src/main.js
+++ src/main.js
@@ -1,40 +1,47 @@
-var request = require('request');
-
 // Global Variables
 var send_button = document.getElementById('message-send-button');
 var text = document.getElementById('message-text');
 var list = document.getElementById('message-data');
 var reply = "";
+var url = "http://api.asksusi.com/susi/chat.json?q=";
+
+function checkResponse(response) {
+  if(response.status===200) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function getJSON(response) {
+  return response.json();
+}
 
 var send_message = function(message) {
-  request({
-    url: "http://api.asksusi.com/susi/chat.json?q=" + encodeURI(message),
-    json: true
-  }, function (err, response, data) {
-    if (!err && response.statusCode === 200) {
-      reply = data.answers[0].actions[0].expression;
-      return true;
-    } else {
-      reply = "Sorry, seems like there is an error.";
-      return false;
-    }
-  });
+  fetch(url + encodeURI(message))
+    .then(checkResponse)
+    .then(getJSON)
+    .then(function(data) {
+      reply = data.answers[0].data[0].answer;
+    })
+    .catch(function(err) {
+      reply = "Sorry. There seems to be a problem."
+    })
 }
 
 var append = function(text, attr){
-  let el = document.createElement("li");
+  let el = document.createElement("div");
   el.setAttribute(attr.name, attr.val);
   el.appendChild(document.createTextNode(text));
   list.appendChild(el);
+  let clear = document.createElement("div");
+  clear.setAttribute("class", "clear");
+  list.appendChild(clear);
 }
 
 send_button.addEventListener('click', function() {
   let message = text.value;
   send_message(message);
-  console.log(reply)
-  append(message, { name: "class", val: "message-by-user" });
-  if(typeof(reply) != undefined){
-    append(reply, { name: "class", val: "message-by-susi" })
-  }
-
+  append(message, { name: "class", val: "from-me" });
+  append(reply, { name: "class", val: "from-them" })
 });
