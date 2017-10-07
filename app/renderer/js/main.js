1ee95bfb4dfd8df605403123c5f99b20110f3fba
Anshuman <anshu.av97@gmail.com>
2017-10-07 10:27:26 +0530

    Fixed some issues, removed sample print statements

--- app/renderer/js/main.js
+++ app/renderer/js/main.js
@@ -1,18 +1,11 @@
-onload = function() {
-
+onload = function () {
 	const webview = document.querySelector('webview');
 	const loading = document.querySelector('#loading');
 
 	function onStopLoad() {
-		console.log('finished');
 		loading.style.display = 'none';
-	}
-
-	function onStartLoad() {
-		console.log('started');
 	}
 
 	webview.addEventListener('did-stop-loading', onStopLoad);
 	webview.addEventListener('did-start-loading', onStartLoad);
-
-}
+};
