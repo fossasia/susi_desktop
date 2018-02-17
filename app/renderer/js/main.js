765f3bc5a995327a713cc7a69b42ca20a336db95
Mohit Sharma <msvbs98@gmail.com>
2018-02-18 00:39:07 +0530

    Fixes #105 #106 #107: yarn is added, now links open in default browser and electron is upadted to latest version
    Fix Travis and Codacy
    
    Add package-lock.json in gitignore

--- app/renderer/js/main.js
+++ app/renderer/js/main.js
@@ -1,10 +1,18 @@
 onload = function () {
+	const {shell} = require('electron');
 	const webview = document.querySelector('webview');
 	const loading = document.querySelector('#loader');
 
 	function onStopLoad() {
 		loading.classList.add('hide');
 	}
+
+	webview.addEventListener('new-window', e => {
+		const protocol = require('url').parse(e.url).protocol;
+		if (protocol === 'http:' || protocol === 'https:') {
+			shell.openExternal(e.url);
+		}
+	});
 
 	function onStartLoad() {
 		loading.classList.remove('hide');
